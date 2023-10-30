a6f0f9501e12a3024a12b309b478fb0a75b983a1
Jakob Engwall <jakob.engwall@gmail.com>
2023-10-30 13:23:48 +0100

    Fifth commit (Trying new Button types)

--- src/Gui.java
+++ src/Gui.java
@@ -9,9 +9,10 @@ public class Gui extends JFrame implements ActionListener {
     JPanel panel = new JPanel(new BorderLayout());
     JPanel northPanel = new JPanel();
     JPanel southPanel = new JPanel();
-
     JButton newGameButton = new JButton("New game");
+    private JButton[][] buttons;
     ArrayList<JButton> buttonList = new ArrayList<>();
+    JButton numberButton;
 
 
     public Gui(){
@@ -19,15 +20,23 @@ public class Gui extends JFrame implements ActionListener {
         panel.add(northPanel,BorderLayout.NORTH);
 
         panel.add(southPanel,BorderLayout.SOUTH);
+
+        buttons = new JButton[4][4];
         southPanel.setLayout(new GridLayout(4,4));
 
         northPanel.add(newGameButton);
 
-        for (int i = 0; i < 15; i++) {
-            JButton numberButton = new JButton("" + (i+1));
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+
+            }
+            numberButton = new JButton("" + (i+1));
             buttonList.add(numberButton);
             southPanel.add(numberButton);
         }
+
+        numberButton.addActionListener(this::actionPerformed);
 
         pack();
         setLocationRelativeTo(null);
@@ -38,6 +47,8 @@ public class Gui extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        revalidate();
+        repaint();
     }
 }
 
