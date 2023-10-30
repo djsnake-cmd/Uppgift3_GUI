0465688fbaf0a83ac55586d11d84655369a25f61
Karl Engwall <karlengwall@gmail.com>
2023-10-30 16:22:02 +0100

    added comments for clarity

--- src/Gui.java
+++ src/Gui.java
@@ -16,8 +16,7 @@ public class Gui extends JFrame implements ActionListener {
     JButton emptyButton = new JButton("");
     ArrayList<JButton> buttonList = new ArrayList<>();
     JButton numberButton;
-
-    private Point emptyLocal = new Point();
+    private Point emptyLocal = new Point(); //Point för att peka var den tomma rutan är på griden
 
 
     public Gui() {
@@ -33,7 +32,7 @@ public class Gui extends JFrame implements ActionListener {
         emptyButton.addActionListener(this);
 
         int count = 1;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { //Kollar varje square i griden och lägger ut siffror i squares
             for (int j = 0; j < 4; j++) {
                 if (count <= 15) {
                     numberButton = new JButton("" + (count));
@@ -43,7 +42,7 @@ public class Gui extends JFrame implements ActionListener {
                     numberButton.addActionListener(this);
                     count++;
 
-                } else {
+                } else { //Lägger ut den sista, tomma, squaren
                     buttons[i][j] = emptyButton;
                     buttonList.add(emptyButton);
                     southPanel.add(emptyButton);
@@ -60,31 +59,30 @@ public class Gui extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == newGameButton) {
+        if (e.getSource() == newGameButton) { //Klickades det på newGameButton? Isf shuffla
             randomizeButtons();
-        } else {
-            for (int i = 0; i < 4; i++) {
+        } else { //Klickades något annat än nGB? Kör nedanstående
+            for (int i = 0; i < 4; i++) { //Kollar varje square i grid.
                 for (int j = 0; j < 4; j++) {
-                    if (e.getSource() == buttons[i][j] && adjacent(i, j)) {
-                        swap(i, j);
+                    if (e.getSource() == buttons[i][j] && adjacent(i, j)) { //Om klickad square är i,j och om square är bredvid tom square (adjacent)
+                        swap(i, j); //Om true, byt plats
                         return;
                     }
                 }
             }
         }
     }
-
-    private boolean adjacent(int x, int y) {
-        return Math.abs(x - emptyLocal.x) + Math.abs(y - emptyLocal.y) == 1;
+    private boolean adjacent(int x, int y) { //Metod för att avgöra om en ruta är bredvid den tomma rutan.
+        return Math.abs(x - emptyLocal.x) + Math.abs(y - emptyLocal.y) == 1; // Math.abs-metoden tar bort kravet - eller + och avgör bara om den tomma rutan är bredvid på en x, y axis
     }
 
     private void swap(int x, int y) {
-        JButton temp = buttons[x][y];
-        buttons[x][y] = buttons[emptyLocal.x][emptyLocal.y];
-        buttons[emptyLocal.x][emptyLocal.y] = temp;
+        JButton temp = buttons[x][y]; //Skapar temporär placeholder för x,y
+        buttons[x][y] = buttons[emptyLocal.x][emptyLocal.y]; //Gör buttons[][] tom
+        buttons[emptyLocal.x][emptyLocal.y] = temp; //Fyller med temp
 
         southPanel.removeAll();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { //Lägger tillbaka alla squares i den nya ordningen
             for (int j = 0; j < 4; j++) {
                 southPanel.add(buttons[i][j]);
             }
@@ -92,20 +90,20 @@ public class Gui extends JFrame implements ActionListener {
         }
         southPanel.revalidate();
         southPanel.repaint();
-        emptyLocal.setLocation(x, y);
+        emptyLocal.setLocation(x, y); //Kommer ihåg ny tom square
 
     }
 
     private void randomizeButtons() {
         southPanel.removeAll();
-        Collections.shuffle(buttonList);
+        Collections.shuffle(buttonList); //Collections för att blanda om squares
 
         int index = 0;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { //Sätter ut alla squares
             for (int j = 0; j < 4; j++) {
                 buttons[i][j] = buttonList.get(index++);
                 southPanel.add(buttons[i][j]);
-                if (buttons[i][j] == emptyButton) {
+                if (buttons[i][j] == emptyButton) { //Sätter ut tom square
                     emptyLocal.setLocation(i, j);
                 }
             }
